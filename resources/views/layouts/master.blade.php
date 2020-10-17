a23ba9f1ef9890d0edb7b1762faa3041798e019c
masadi <masadi.com@gmail.com>
2020-10-17 09:09:37 +0700

    Update master.blade.php

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -112,10 +112,10 @@
   <footer class="main-footer">
     {{-- To the right --}}
     <div class="float-right d-none d-sm-block">
-      <b>Version</b> 3.0.0
+      <b>Version</b> 1.0.0
     </div>
     {{-- Default to the left --}}
-    <strong>Copyright &copy; 2014-2019 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+    <strong>Copyright &copy; {{date('Y')}} <a href="http://mubakid.or.id/">PPMU Bakid</a>.</strong> All rights reserved.
   </footer>
 </div>
 {{-- ./wrapper --}}
